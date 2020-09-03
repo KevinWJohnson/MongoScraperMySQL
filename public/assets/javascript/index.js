fa2be74ad95a84094c3c3d87e1aef56f742d3fb4
Kevin Johnson <kevin.johnson3@cox.net>
2020-09-03 13:52:10 -0700

    Saved the files that changed _id to id

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
         $("#notes").append("<textarea id='bodyinput' name='body' placeholder='Enter Note Here'></textarea>");
         // A button to submit a new note, with the id of the article saved to it
         $("#notes").append(
-          "<button data-id='" + data._id + "' id='savenote'>Save Note</button>"
+          "<button data-id='" + data.id + "' id='savenote'>Save Note</button>"
         );
 
 
